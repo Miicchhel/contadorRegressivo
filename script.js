609dc30796ab684cc4835ac1d933c9bae20c72e8
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-08-06 18:05:06 -0300

    Timer funcional 1.0

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const startTimer = (duracao, display) => {
   let tempo = duracao;
   const intervalo = setInterval(() => {
-    console.log('entrou');
     let min = parseInt(tempo / 60);
     let seg = parseInt(tempo % 60);
 
@@ -10,13 +9,36 @@ const startTimer = (duracao, display) => {
 
     display.textContent = min + ":" + seg;
     
-    (tempo === 0) ? clearInterval(intervalo) : tempo -= 1;
+    if (tempo === 0) {
+      clearInterval(intervalo)
+      const inicio = document.getElementById('inicio')
+      inicio.style.display = 'flex';
+      display.style.display = 'none';
+
+    } else {
+      tempo -= 1;
+    }
 
   }, 1000);
 }
 
 window.onload = () => {
-  var duracao = 60; //conversão para segundos
-  var display = document.querySelector("#timer");
-  startTimer(duracao, display);
+  const inicio = document.getElementById('inicio')
+  const btnTimer = document.getElementById('btnTimer');
+  btnTimer.addEventListener('click', ()=>{
+    let inputMinutos = document.getElementById('inputMinutos').value;
+    let inputSegundos = document.getElementById('inputSegundos').value;
+
+    if (!inputMinutos) inputMinutos = 0;
+    if (!inputSegundos) inputSegundos = 0;
+
+    const tempo = parseInt(inputSegundos) + parseInt(inputMinutos)*60;
+    var duracao = tempo; //conversão para segundos
+    var display = document.querySelector("#timer");
+    inicio.style.display = 'none';
+    display.style.display = 'block';
+    startTimer(duracao, display);
+
+  });
+
 }
